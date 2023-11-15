e5695aeea7c7324212329e040b6fec6588714f79
JoXW100 <joxw100@gmail.com>
2023-11-15 20:00:44 +0100

    Simplified GameObject.position()

--- src/engine/GameObject.cpp
+++ src/engine/GameObject.cpp
@@ -7,8 +7,7 @@ GameObject::GameObject(const Vector3 &pos) : m_position(pos) {}
 GameObject::~GameObject() {}
 
 admirals::Vector3 GameObject::position() const {
-    Vector3 position = m_position; // A copy
-    return position;
+    return m_position; // A copy
 }
 
 void GameObject::setPosition(const Vector3 &pos) { this->m_position = pos; }
