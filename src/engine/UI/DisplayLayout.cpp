a4aa0ee8b6d58b9680c6cc4c7463e0652cb9a7a3
Joel Sikström <joel.attano@gmail.com>
2023-11-16 19:20:05 +0100

    Moved debug rendering after element rendering.

--- src/engine/UI/DisplayLayout.cpp
+++ src/engine/UI/DisplayLayout.cpp
@@ -44,13 +44,13 @@ void DisplayLayout::render(const renderer::RendererContext &r) const {
 
         element->SetDisplayOrigin(origin);
 
+        element->Render(this->m_font);
+
         // If debugging, render an outline around the UI Element.
         if (r.renderDebugOutlines) {
             renderer::Renderer::drawRectangleOutline(origin, displaySize, 2,
                                                      Color::RED);
         }
-
-        element->Render(this->m_font);
 
         // Update the matching positionOffset.
         if (pos == DisplayPosition::UpperRight ||
