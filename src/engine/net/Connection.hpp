6cdf3d96df05c985d8e376564ff65d95a04d37ee
Casper Norrbin <61315185+caspernorrbin@users.noreply.github.com>
2023-11-17 13:57:28 +0100

    Initial client/server networking (#36)
    * Initial client/server networking
    
    * clang formatting
    
    * CI fix
    
    * clang format on CI fix
    
    * Removed Message id enum, changed .tpp to .cpp
    
    * applied format
    
    * renamed member variables
    
    * Increased readability from feedback
    
    * Networking bound handlers (#40)
    
    * Bound handlers
    - Changed lanbda functions to directly bind to handlers
    - FIxed _WIN32_WINNT not defined warnings
    
    * Added clarifying comment for _WIN32_WINNT definition
    
    * Added check before defining _WIN32_WINNT
    
    * Applied format and improved _WIN32_WINNT check
    
    * Build error fix
    
    * removed lambda function and old comment
    
    ---------
    
    Co-authored-by: JoXW100 <joxw100@gmail.com>
    Co-authored-by: Jakob Wallén <57936645+JoXW100@users.noreply.github.com>

---
+++ src/engine/net/Connection.hpp
@@ -0,0 +1,91 @@
+#pragma once
+#ifndef ASIO_STANDALONE
+#define ASIO_STANDALONE 1
+#endif
+#if !defined(_WIN32_WINNT) && (defined(WIN32) || defined(_WIN32))
+#define _WIN32_WINNT 0x0A00 // Windows 10
+#endif
+#include <asio.hpp>
+
+#include "Message.hpp"
+#include "MessageQueue.hpp"
+
+// Can be changed to disallow connections from different versions
+#define CONNECTION_VERSON 0
+
+namespace admirals::net {
+
+using ValidationFunction = std::function<void(std::shared_ptr<Connection>)>;
+
+class Server;
+
+class Connection : public std::enable_shared_from_this<Connection> {
+public:
+    // Distinguishes between a server and a client
+    enum class Owner { SERVER, CLIENT };
+
+    Connection(Owner parent, asio::io_context &io_context,
+               asio::ip::tcp::socket socket,
+               MessageQueue<OwnedMessage> &incoming_messages,
+               Server *server = nullptr,
+               ValidationFunction validation_function = nullptr);
+    virtual ~Connection();
+
+    // Connects to a client from the server
+    void ConnectToClient(admirals::net::Server *server, uint32_t uid = 0);
+
+    // Connects to the server from a client
+    void
+    ConnectToServer(const asio::ip::tcp::resolver::results_type &endpoints);
+
+    // Disconnects the connection
+    bool Disconnect();
+
+    // Returns true if the connection is open
+    bool IsConnected() const;
+
+    // Sends a message through the connection
+    void Send(const Message &message);
+
+    // Returns the id of the connection
+    // Only applicable for client ids
+    uint32_t GetID() const;
+
+private:
+    void ReadHeader();
+    void ReadBody();
+    void AddToIncomingMessageQueue();
+    void WriteHeader();
+    void WriteBody();
+    uint64_t HashValidation(uint64_t hash);
+    void WriteValidation();
+    void ReadValidation();
+
+    void HandleConnectToserver(std::error_code ec);
+    void HandleReadHeader(std::error_code ec);
+    void HandleReadBody(std::error_code ec);
+    void HandleWriteHeader(std::error_code ec);
+    void HandleWriteBody(std::error_code ec);
+    void HandleWriteValidation(std::error_code ec);
+    void HandleReadValidation(std::error_code ec);
+
+private:
+    asio::io_context &m_ioContext;
+    asio::ip::tcp::socket m_socket;
+
+    MessageQueue<Message> m_outgoingMessages;
+    MessageQueue<OwnedMessage> &m_incomingMessages;
+    Message m_messageBuffer;
+
+    Owner m_ownerType = Owner::SERVER;
+    uint32_t m_id = 0;
+
+    uint64_t m_validationOut = 0;
+    uint64_t m_validationIn = 0;
+
+    // Used when validating a connection
+    Server *m_server = nullptr;
+    ValidationFunction m_validationFunction = nullptr;
+};
+
+} // namespace admirals::net
