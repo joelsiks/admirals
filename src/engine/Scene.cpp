20257d6c929443ade997bd5de3b0596a342793e2
Oskar <104132731+OskarG0@users.noreply.github.com>
2023-11-20 13:58:43 +0100

    Basic imlementation of Scene (#20)
    * Basic imlementation of Scene
    
    implemented the structure for scen  to add and remove gameObjects, together with tests of the functions.
    
    * applied clang format
    
    applied clang format
    
    * rework of scene
    
    remade how the gameObject are found in the scene list so it goes after the pointer of the gameobject and the index of said object.
    due to change the duplicate function was removed.
    tests was also remade
    
    * Update Scene.cpp
    
    fixed objects -> m_objects
    
    * Applied formating
    
    * fixed issues with scene
    
    replaced idnex function with position and its z value.
    updated the test file to use the new game object structure
    
    * Refactor of Scene
    
    remade scene to use OrderedCollection instead of multiset.
    changed tests to accomaodate for this change
    
    * clang formatting
    
    ---------
    
    Co-authored-by: JoXW100 <joxw100@gmail.com>

--- src/engine/Scene.cpp
+++ src/engine/Scene.cpp
@@ -1,4 +1,5 @@
 #include "Scene.hpp"
+#include <stdio.h>
 
 using namespace admirals::scene;
 
@@ -10,6 +11,46 @@ void Scene::Render(const renderer::RendererContext &r) const {
     for (const auto &object : this->m_objects) {
         object->Render(r);
     }
+}
+
+/**
+ * recieves a shared pointer to a gameObject and removes one from the multiset
+ */
+void Scene::RemoveObject(std::shared_ptr<GameObject> object) {
+    if (ExistObject(object)) {
+        this->m_objects.Erase(object->name());
+    }
+}
+void Scene::RemoveObject(const std::string &key) { this->m_objects.Erase(key); }
+
+/**
+ * recieves a shared pointer to a gameObject and checks if it exist in the
+ * multiset
+ */
+bool Scene::ExistObject(std::shared_ptr<GameObject> object) {
+    auto obj = this->m_objects.Find(object->name());
+    if (obj != nullptr) {
+        // if another that doesn't exist in m_objects have an identical name to
+        // on e that does. check if the pointer is the same as the desired
+        // value.
+        if (obj == object) {
+            return true;
+        }
+    }
+    return false;
+}
+bool Scene::ExistObject(const std::string &key) {
+    return (this->m_objects.Find(key) != nullptr);
+}
+
+int Scene::NumObjectsInScene() { return this->m_objects.Size(); }
+
+std::vector<std::string> Scene::GetSceneObjectNames() {
+    std::vector<std::string> vec = {};
+    for (const auto &value : this->m_objects) {
+        vec.emplace_back(value->name());
+    }
+    return vec;
 }
 
 void Scene::OnStart() {
