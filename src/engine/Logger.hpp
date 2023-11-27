4c345991bd18fee3c608f32df2a3c83aa7372471
OskarG0 <oskar.gadin@hotmail.com>
2023-11-27 14:31:42 +0100

    Clang format
    formated after clang

--- src/engine/Logger.hpp
+++ src/engine/Logger.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <ctime>
-#include <iostream>
-#include <fstream>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
 namespace admirals {
 
@@ -17,15 +17,31 @@ private:
         const std::time_t currentTime = std::time(nullptr);
         long long time_taken = currentTime - startTime_m;
         long sec = time_taken % 60;
-        long min = (time_taken / 60 ) % 60;
+        long min = (time_taken / 60) % 60;
         long hour = (time_taken / 3600) % 60;
-        std::string return_str = std::to_string(hour) 
-                        + ":" + std::to_string(min) 
-                        + ":" + std::to_string(sec);
+        std::string return_str = std::to_string(hour) + ":" +
+                                 std::to_string(min) + ":" +
+                                 std::to_string(sec);
         return return_str;
     }
+
+    std::string getErrorMessage(std::error_code ec) {
+        switch (ec.value()) {
+        case 1337:
+            return "One code paragraph was too elite.";
+        case 101:
+            return "Engine Error: \n    Value out of bounds";
+        case 102:
+            return "Engine Error: \n    Object not found";
+        case 201:
+            return "Network error:\n    Connection lost";
+        default: // code 0
+            return "unknown error code.";
+        }
+    }
+
 public:
-    Logger(){
+    Logger() {
         textlog_m.open(filename_m);
         textlog_m.close();
     };
@@ -36,42 +52,22 @@ public:
     void catchError(const std::string &logString, int ec) {
         catchError(logString, std::error_code(ec, std::system_category()));
     }
-    void catchError(std::error_code ec) {
-        catchError("NO MESSAGE", ec);
-    }
+    void catchError(std::error_code ec) { catchError("NO MESSAGE", ec); }
     void catchError(const std::string &logString) {
-        catchError(logString, std::error_code()); //error code 0
+        catchError(logString, std::error_code()); // error code 0
     }
     void catchError(const std::string &logString, std::error_code ec) {
         textlog_m.open(filename_m, std::ios::app);
         textlog_m << std::asctime(std::localtime(&startTime_m))
-                << "time since start: " + (timeSinceStart()) + "\n"
-                << getErrorMessage(ec) + "\n"
-                << "---Unique log message: \n"
-                << logString + "\n"
-                << "---End of Message \n"
-                << std::filesystem::current_path(ec)
-                << ("\n\n");
+                  << "time since start: " + (timeSinceStart()) + "\n"
+                  << getErrorMessage(ec) + "\n"
+                  << "---Unique log message: \n"
+                  << logString + "\n"
+                  << "---End of Message \n"
+                  << std::filesystem::current_path(ec) << ("\n\n");
 
         textlog_m.close();
     }
-
-    std::string getErrorMessage(std::error_code ec) {
-        switch (ec.value())
-        {
-        case 1337:
-            return "One code paragraph was too elite.";
-        case 101:
-            return "Engine Error: \n    Value out of bounds";
-        case 102:
-            return "Engine Error: \n    Object not found";
-        case 201:
-            return "Network error:\n    Connection lost";
-        default: //code 0
-            return "unknown error code.";
-        }
-    }
-
 };
 
 } // namespace admirals
--- src/test/Logger.test.cpp
+++ src/test/Logger.test.cpp
@@ -1,8 +1,8 @@
+#include <chrono>
 #include <ctime>
 #include <iostream>
 #include <stdbool.h>
 #include <stdio.h>
-#include <chrono>
 #include <thread>
 
 #include "Logger.hpp"
@@ -14,8 +14,9 @@ int main(int argc, char *argv[]) {
     Logger *logWriter = new Logger();
 
     logWriter->catchError("error found");
-    std::this_thread::sleep_for (std::chrono::seconds(2));
-    logWriter->catchError("new line", std::error_code(1337, std::system_category()));
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    logWriter->catchError("new line",
+                          std::error_code(1337, std::system_category()));
     logWriter->catchError(std::error_code(101, std::system_category()));
 
     return EXIT_SUCCESS;
