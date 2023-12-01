bfbadf4546f446dd2f21162b25fbaf5c14c253cd
OskarG0 <oskar.gadin@hotmail.com>
2023-12-01 13:42:32 +0100

    reworked logger

--- src/engine/Logger.hpp
+++ src/engine/Logger.hpp
@@ -14,14 +14,13 @@ public:
     enum ErrorCase {
         ValueOutOfBound = 101,
         NoObjectFound = 102,
+        DataMissing = 103,
         NetworkConnectionLost = 201
     };
+    enum WarningCase { MismatchOfValues = 101, WrongIdentifiers = 102 };
 
 private:
-    static constexpr std::string m_filename = "logFile.txt";
-
     Logger(){};
-
     static std::string GetCurrentTime() {
         const std::time_t time = std::time(nullptr);
         std::string time_string(std::asctime(std::localtime(&time)));
@@ -31,19 +30,32 @@ private:
     static std::string GetErrorMessage(ErrorCase ec) {
         switch (ec) {
         case ValueOutOfBound:
-            return "Engine Error: \n    Value out of bounds";
+            return "Error:\n  Value out of bounds";
         case NoObjectFound:
-            return "Engine Error: \n    Object not found";
+            return "Error:\n  Object not found";
+        case DataMissing:
+            return "Error:\n  Data missing";
         case NetworkConnectionLost:
-            return "Network error:\n    Connection lost";
+            return "Error:\n  Connection lost";
         default: // code 0
             return "unknown error code.";
         }
     }
 
+    static std::string GetWarningMessage(WarningCase ec) {
+        switch (ec) {
+        case MismatchOfValues:
+            return "Warning:\n  Possible mismatch of varaiables";
+        case WrongIdentifiers:
+            return "Warning:\n  Wrong Inditifiers";
+        default: // code 0
+            return "unknown warning code.";
+        }
+    }
+
     static void AppendToLogFile(const std::string &text) {
         std::ofstream file1;
-        file1.open(m_filename, std::ios::app);
+        file1.open("logFile.txt", std::ios::app);
         file1 << GetCurrentTime();
         file1 << text + "\n\n";
         file1.close();
@@ -52,7 +64,7 @@ private:
 public:
     static void ClearLogFile() {
         std::ofstream file1;
-        file1.open(m_filename);
+        file1.open("logFile.txt");
         file1.close();
     }
 
@@ -62,6 +74,13 @@ public:
 
     static void CatchError(const std::string &logString, ErrorCase ec) {
         const std::string text = "\n" + GetErrorMessage(ec) + "\n" +
+                                 "---Unique log message: \n" + logString +
+                                 "\n" + "---End of Message \n";
+        AppendToLogFile(text);
+    }
+
+    static void CatchWarning(const std::string &logString, WarningCase ec) {
+        const std::string text = "\n" + GetWarningMessage(ec) + "\n" +
                                  "---Unique log message: \n" + logString +
                                  "\n" + "---End of Message \n";
         AppendToLogFile(text);
