2dde5a52959332d96afcbb2931d136affe88839c
Joel Sikström <joel.attano@gmail.com>
2023-12-14 15:26:10 +0100

    Moved menu updates from Render to Update.

--- src/engine/UI/menu/Menu.cpp
+++ src/engine/UI/menu/Menu.cpp
@@ -18,11 +18,47 @@ Menu::Menu(const std::string &menuTitle, const Color &foregroundColor,
 }
 
 void Menu::Render(const EngineContext &ctx) const {
-    float centerPositionOffset = m_topPadding;
-
     // Draw background.
     renderer::Renderer::DrawRectangle(Vector2(0, 0), ctx.windowSize, m_bgColor);
 
+    for (auto it = m_displayables.rbegin(); it != m_displayables.rend(); ++it) {
+        if (*it == nullptr) {
+            continue;
+        }
+
+        auto element = std::dynamic_pointer_cast<UI::Element>(*it);
+        auto option = std::dynamic_pointer_cast<menu::MenuOption>(element);
+
+        const Vector2 position = option->GetPosition();
+        const Vector2 displaySize = option->GetSize();
+
+        option->Render(ctx);
+
+        // If we're rendering the menu title, add a line below it.
+        if (option->identifier() == MENU_TITLE_NAME) {
+            renderer::Renderer::DrawLine(
+                Vector2(position[0], position[1] + displaySize[1]),
+                Vector2(position[0] + displaySize[0],
+                        position[1] + displaySize[1]),
+                m_fgColor);
+        }
+
+        // If debugging, render an outline around the UI Element.
+        if (ctx.debug) {
+            renderer::Renderer::DrawRectangleOutline(position, displaySize, 2,
+                                                     Color::RED);
+        }
+    }
+
+    if (ctx.debug) {
+        m_quadtree.DrawTree();
+    }
+}
+
+void Menu::Update(const EngineContext &ctx) {
+    IDisplayLayer::Update(ctx);
+
+    float centerPositionOffset = m_topPadding;
     for (auto it = m_displayables.rbegin(); it != m_displayables.rend(); ++it) {
         if (*it == nullptr) {
             continue;
@@ -36,6 +72,11 @@ void Menu::Render(const EngineContext &ctx) const {
         // orientation, even if it is changed externally.
         option->SetDisplayOrientation(DisplayOrientation::Center);
 
+        // Update menu-dependent state of the options.
+        option->SetSize(renderer::Renderer::TextFontSize(
+            option->GetOptionText(), ctx.fontWidth, ctx.fontHeight));
+        option->SetTextColor(m_fgColor);
+
         const DisplayOrientation orientation = option->GetDisplayOrientation();
         Vector2 displaySize = option->GetSize();
 
@@ -44,39 +85,15 @@ void Menu::Render(const EngineContext &ctx) const {
             GetPositionFromOrientation(orientation, displaySize, ctx);
         position[1] += centerPositionOffset;
 
-        // Update menu-dependent state of the options.
-        option->SetSize(renderer::Renderer::TextFontSize(
-            option->GetOptionText(), ctx.fontWidth, ctx.fontHeight));
-        option->SetTextColor(m_fgColor);
-
         option->SetPosition(position);
-        option->Render(ctx);
 
         // If we're rendering the menu title, add a line below it.
         if (option->identifier() == MENU_TITLE_NAME) {
-            renderer::Renderer::DrawLine(
-                Vector2(position[0], position[1] + displaySize[1]),
-                Vector2(position[0] + displaySize[0],
-                        position[1] + displaySize[1]),
-                m_fgColor);
             centerPositionOffset += MENU_TITLE_LINE_OFFSET;
-        }
-
-        // If debugging, render an outline around the UI Element.
-        if (ctx.debug) {
-            renderer::Renderer::DrawRectangleOutline(position, displaySize, 2,
-                                                     Color::RED);
         }
 
         centerPositionOffset += displaySize[1];
     }
 
-    if (ctx.debug) {
-        m_quadtree.DrawTree();
-    }
-}
-
-void Menu::Update(const EngineContext &ctx) {
-    IDisplayLayer::Update(ctx);
     RebuildQuadTree(ctx.windowSize);
 }
